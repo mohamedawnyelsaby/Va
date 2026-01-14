923852c7265a43af2510ec0eaae80bebe9be6e29
Mohamed awny <55304205+mohamedawnyelsaby@users.noreply.github.com>
2026-01-14 20:32:05 +0200

    Update index.tsx

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -3,16 +3,16 @@ import { ThemeProvider } from './theme-provider';
 import { QueryProvider } from './query-provider';
 import { I18nProvider } from './i18n-provider';
 import { PiProvider } from './pi-provider';
-import { ToastProvider } from './toast-provider';
+import ToastProvider from './toast-provider';
 import { ModalProvider } from './modal-provider';
 import { TooltipProvider } from './tooltip-provider';
 
 interface ProvidersProps {
   children: React.ReactNode;
-  locale?: string; 
+  locale?: string;
 }
 
-export function Providers({ children, locale = 'en' }: ProvidersProps) { // 👈 وده
+export function Providers({ children, locale = 'en' }: ProvidersProps) {
   return (
     <ThemeProvider
       attribute="class"
@@ -21,14 +21,13 @@ export function Providers({ children, locale = 'en' }: ProvidersProps) { // 👈
       disableTransitionOnChange
     >
       <QueryProvider>
-        <I18nProvider locale={locale}> {/* 👈 وده المهم جداً */}
+        <I18nProvider locale={locale}>
           <PiProvider>
             <TooltipProvider>
-              <ToastProvider>
-                <ModalProvider>
-                  {children}
-                </ModalProvider>
-              </ToastProvider>
+              <ModalProvider>
+                {children}
+              </ModalProvider>
+              <ToastProvider />
             </TooltipProvider>
           </PiProvider>
         </I18nProvider>
