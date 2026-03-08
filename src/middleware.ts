7f90f751abce69490453efb2fccd07b87772ae28
Mohamed awny <55304205+mohamedawnyelsaby@users.noreply.github.com>
2026-03-08 22:05:19 +0200

    Update matcher configuration in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,5 +37,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api/|_next/static|_next/image|favicon|apple-touch|site.webmanifest|robots|sitemap).*)',
+  ],
 };
